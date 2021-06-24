202849941a531cab4eadf06be660461b2a1d3d5f
IMRANDIL <aliimranadil2@gmail.com>
2021-06-24 11:17:19 +0530

    Updates

---
+++ precache-manifest.f1cfad3f0d45e15ee80a42099b0d4359.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcceeb048cc2a1a2186e72a554ffb3cc",
+    "url": "/Color_Generator_React/index.html"
+  },
+  {
+    "revision": "d254d2140ad07d6b5267",
+    "url": "/Color_Generator_React/static/css/main.9f3b6256.chunk.css"
+  },
+  {
+    "revision": "0b728d42613a7c9ec6d3",
+    "url": "/Color_Generator_React/static/js/2.cdd6e70d.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/Color_Generator_React/static/js/2.cdd6e70d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d254d2140ad07d6b5267",
+    "url": "/Color_Generator_React/static/js/main.c6eab0c1.chunk.js"
+  },
+  {
+    "revision": "b579f76da99e33be2c2a",
+    "url": "/Color_Generator_React/static/js/runtime-main.b0019add.js"
+  }
+]);
